8a3837df18c3c0367613b906a2ac8adfeada193a
raauf <vrairaaufabidi@gmail.com>
2022-03-02 12:20:26 +0100

    dernier modification

--- pointeur1.c
+++ pointeur1.c
@@ -36,4 +36,36 @@ int* gradingStudents(int grades_count, int* grades, int* result_count) {
         return nouveauGrade;
     
 }
+_______________________________
+char* kangaroo(int x1, int v1, int x2, int v2) {
+    int firstKanguru, secondKenguru;
+    int theSame=-1, intervalle;
+    static char res[10];
+    if(x1<x2 && x1>=0 && x2<=10000 && v1>=1 && v1<=10000 && v2>=1 && v2<=10000){
+        intervalle=x1;
+        
+        while(theSame==-1 && intervalle<=10000){
+            firstKanguru=x1+v1;
+            secondKenguru=x2+v2;
+            if(firstKanguru==secondKenguru){
+                theSame=1;
+                break;
+            }else if(firstKanguru>secondKenguru)
+                intervalle+=firstKanguru;
+            else 
+                intervalle+=secondKenguru;
+            x1=firstKanguru;
+            x2=secondKenguru;
+        }
+        if(theSame==-1)
+            strcpy(res, "NO");
+        else
+            strcpy(res, "YES");
+    
+    }else{
+        strcpy(res, "NO");
+    }
+    return res;
+}
+
 */
